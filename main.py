f6cc3b3b78580aca6fc9e9643701b28aea7c8a00
Thais Fialho <thaisfialho301@gmail.com>
2025-10-29 17:19:00 -0300

    Adicionado o arquivo main com a função de login

---
+++ main.py
@@ -0,0 +1,19 @@
+from selenium import webdriver
+from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.chrome.service import Service
+import data
+from page import ServeRestPage
+
+class TestServeRest:
+
+    def test_set_login(self):
+        self.driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
+        self.driver.get(data.ServeRest_URL)
+        self.serveRest = ServeRestPage(self.driver)
+        email = data.EMAIL
+        password = data.PASSWORD
+        self.serveRest.set_login(email, password)
+        #self.serveRest.set_enter()
+        assert self.serveRest.get_email_field() == email
+        assert self.serveRest.get_password_field() == password
+        self.driver.quit()
